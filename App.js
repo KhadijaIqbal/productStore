c25d9240914bc21e6b1000739749298ff3dad178
KhadijaIqbal <deejaiqbal.19@gmail.com>
2021-05-20 03:24:43 +0500

    my new commit

--- App.js
+++ App.js
@@ -1,370 +1,287 @@
 import React, { useState } from 'react';
 import {
   Text,
-  View,
   StyleSheet,
   Button,
-  TouchableWithoutFeedback,
-  Pressable,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView, 
 } from 'react-native';
-import {Modal} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const masterboard = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
-];
-export default function App() {
-  const [getTxt1, setTxt1] = useState('-');
-  const [getTxt2, setTxt2] = useState('-');
-  const [getTxt3, setTxt3] = useState('-');
-  const [getTxt4, setTxt4] = useState('-');
-  const [getTxt5, setTxt5] = useState('-');
-  const [getTxt6, setTxt6] = useState('-');
-  const [getTxt7, setTxt7] = useState('-');
-  const [getTxt8, setTxt8] = useState('-');
-  const [getTxt9, setTxt9] = useState('-');
-  const [getTurn, setTurn] = useState('p1');
-  const [getResult, setResult] = useState('');
-  const [modalVisible, setModalVisible] = useState(false);
-  const onClick = (game) => {
-    if (getTurn == 'p1') {
-      if (game == 1 && getTxt1 == '-') {
-        setTxt1('X');
-        masterboard[0][0] = 'X';
-      }
-      if (game == 2 && getTxt2 == '-') {
-        setTxt2('X');
-        masterboard[0][1] = 'X';
-      }
-      if (game == 3 && getTxt3 == '-') {
-        setTxt3('X');
-        masterboard[0][2] = 'X';
-      }
-      if (game == 4 && getTxt4 == '-') {
-        setTxt4('X');
-        masterboard[1][0] = 'X';
-      }
-      if (game == 5 && getTxt5 == '-') {
-        setTxt5('X');
-        masterboard[1][1] = 'X';
-      }
-      if (game == 6 && getTxt6 == '-') {
-        setTxt6('X');
-        masterboard[1][2] = 'X';
-      }
-      if (game == 7 && getTxt7 == '-') {
-        setTxt7('X');
-        masterboard[2][0] = 'X';
-      }
-      if (game == 8 && getTxt8 == '-') {
-        setTxt8('X');
-        masterboard[2][1] = 'X';
-      }
-      if (game == 9 && getTxt9 == '-') {
-        setTxt9('X');
-        masterboard[2][2] = 'X';
-      }
-      setTurn('p2');
-    } else if (getTurn == 'p2') {
-      if (game == 1 && getTxt1 == '-') {
-        setTxt1('O');
-        masterboard[0][0] = 'O';
-      }
-      if (game == 2 && getTxt2 == '-') {
-        setTxt2('O');
-        masterboard[0][1] = 'O';
-      }
-      if (game == 3 && getTxt3 == '-') {
-        setTxt3('O');
-        masterboard[0][2] = 'O';
-      }
-      if (game == 4 && getTxt4 == '-') {
-        setTxt4('O');
-        masterboard[1][0] = 'O';
-      }
-      if (game == 5 && getTxt5 == '-') {
-        setTxt5('O');
-        masterboard[1][1] = 'O';
-      }
-      if (game == 6 && getTxt6 == '-') {
-        setTxt6('O');
-        masterboard[1][2] = 'O';
-      }
-      if (game == 7 && getTxt7 == '-') {
-        setTxt7('O');
-        masterboard[2][0] = 'O';
-      }
-      if (game == 8 && getTxt8 == '-') {
-        setTxt8('O');
-        masterboard[2][1] = 'O';
-      }
-      if (game == 9 && getTxt9 == '-') {
-        setTxt9('O');
-        masterboard[2][2] = 'O';
-      }
-      setTurn('p1');
-    }
-
-    result();
-  };
-  const reload = () => {
-    window.location.reload(false);
-  };
-  const result = () => {
-    if (
-      (masterboard[0][0] === 'X' && masterboard[0][1] === 'X' && masterboard[0][2] === 'X') ||
-      (masterboard[0][0] === 'X' && masterboard[1][1] === 'X' && masterboard[2][2] === 'X') ||
-      (masterboard[0][0] === 'X' && masterboard[1][0] === 'X' && masterboard[2][0] === 'X') ||
-      (masterboard[0][1] === 'X' && masterboard[1][1] === 'X' && masterboard[2][1] === 'X') ||
-      (masterboard[0][2] === 'X' && masterboard[1][2] === 'X' && masterboard[2][2] === 'X') ||
-      (masterboard[0][2] === 'X' && masterboard[1][1] === 'X' && masterboard[2][0] === 'X') ||
-      (masterboard[0][2] === 'X' && masterboard[1][1] === 'X' && masterboard[2][0] === 'X') ||
-      (masterboard[1][0] === 'X' && masterboard[1][1] === 'X' && masterboard[1][2] === 'X') ||
-      (masterboard[2][0] === 'X' && masterboard[2][1] === 'X' && masterboard[2][2] === 'X')
-    ) {
-      setTurn('');
-      modalDisplay('PLAYER 1 WINS');
-      alert('PLAYER 1 WINS!');
-
-
-    } else if (
-      (masterboard[0][0] === 'O' && masterboard[0][1] === 'O' && masterboard[0][2] === 'O') ||
-      (masterboard[0][0] === 'O' && masterboard[1][1] === 'O' && masterboard[2][2] === 'O') ||
-      (masterboard[0][0] === 'O' && masterboard[1][0] === 'O' && masterboard[2][0] === 'O') ||
-      (masterboard[0][1] === 'O' && masterboard[1][1] === 'O' && masterboard[2][1] === 'O') ||
-      (masterboard[0][2] === 'O' && masterboard[1][2] === 'O' && masterboard[2][2] === 'O') ||
-      (masterboard[0][2] === 'O' && masterboard[1][1] === 'O' && masterboard[2][0] === 'O') ||
-      (masterboard[0][2] === 'O' && masterboard[1][1] === 'O' && masterboard[2][0] === 'O') ||
-      (masterboard[1][0] === 'O' && masterboard[1][1] === 'O' && masterboard[1][2] === 'O') ||
-      (masterboard[2][0] === 'O' && masterboard[2][1] === 'O' && masterboard[2][2] === 'O')
-    ) {
-      setTurn('');
-      setModalVisible(true);
-      modalDisplay('PLAYER 2 WINS');
-      alert('PLAYER 2 WINS');
-    } else if (
-      masterboard[0][0] !== '' &&
-      masterboard[0][1] !== '' &&
-      masterboard[0][2] !== '' &&
-      masterboard[1][0] !== '' &&
-      masterboard[1][1] !== '' &&
-      masterboard[1][2] !== '' &&
-      masterboard[2][0] !== '' &&
-      masterboard[2][1] !== '' &&
-      masterboard[2][2] !== ''
-    ) {
-      setTurn('');
-      setModalVisible(true);
-      modalDisplay('Tis a Draw');
-      alert('Tis a DRAW!');
-    }
-  };
-  const modalDisplay = () =>{
-         <View style={styles.centeredView}>
-    <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          alert("Modal has been closed.");
-          console.log("aaaa");
-          setModalVisible(!modalVisible);
-          modalDisplay();
-        }}
-      ></Modal></View>
-        
-
-  }
-
-  const newGame = () => {
-    setTxt1('-'),
-      setTxt2('-'),
-      setTxt3('-'),
-      setTxt4('-'),
-      setTxt5('-'),
-      setTxt6('-'),
-      setTxt7('-'),
-      setTxt8('-'),
-      setTxt9('-');
-    (masterboard[0][0] = ''),
-      (masterboard[0][1] = ''),
-      (masterboard[0][2] = ''),
-      (masterboard[1][0] = ''),
-      (masterboard[1][1] = ''),
-      (masterboard[1][2] = ''),
-      (masterboard[2][0] = ''),
-      (masterboard[2][1] = ''),
-      (masterboard[2][2] = '');
-    setResult('');
-    setTurn('p1');
-  };
-
-  const [getText, setText] = useState('');
-
-  const buttonClick = (txt) => {
-    // alert(txt)
-    setText(getText + txt);
-  };
-
+const HomePage = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View
-        style={[
-          {
-            padding: '10%',
-            alignItems: 'center',
-            fontWeight: 'bold',
-            fontFamily: 'Roboto',
-            fontSize: '85%',
-            textAlign: 'center',
-          },
-        ]}>
-        <View style={[{ padding: '10%', fontSize: '25' }]}>
-          <Button onPress={reload} title="RELOAD GAME" color="#f78da7" />
-        </View>
-        CURRENT PLAYER :{' '}
-        {getResult == ''
-          ? getTurn == 'p1'
-            ? 'PLAYER 1       SYMBOL: X'
-            : 'PLAYER 2       SYMBOL: O'
-          : ''}
-        <Text />
-      </View>
-      <View style={styles.viewRowContainer}>
-        <Pressable onPress={() => onClick(1)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt1}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(2)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt2}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(3)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt3}</Text>
-          </View>
-        </Pressable>
-      </View>
-      <View style={styles.viewRowContainer}>
-        <Pressable onPress={() => onClick(4)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt4}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(5)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt5}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(6)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt6}</Text>
-          </View>
-        </Pressable>
-      </View>
-      <View style={styles.viewRowContainer}>
-        <Pressable onPress={() => onClick(7)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt7}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(8)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt8}</Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => onClick(9)}>
-          <View style={(styles.buttonContainer, styles.btnSize)}>
-            <Text style={styles.buttonText}>{getTxt9}</Text>
-          </View>
-        </Pressable>
-      </View>
-      <View style={[{ alignItems: 'center' }]}>
-        <Text
-          style={[{ padding: '10%', fontSize: '25', justifyContent: 'center' }]}
-        />
-        {getResult}
-        <Text />
-      </View>
-      <View style={[{ padding: '5%', fontSize: '25' }]}>
-        <Button onPress={newGame} title="NEW GAME" color="#f78da7" />
-      </View>
-
-   
-       <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-           setResult('');
-            <View style={[styles.textStyle]}>
-        <Button onPress={newGame} title="NEW GAME" color="#f78da7" />
-      </View>
-          
-          </View>
-       
+      
      
+<TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("List of Products")}>
+      <Text style= {styles.touchtext}>  MANAGE PRODUCTS </Text>
+      </TouchableOpacity>
+
+<TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("Employee")}>
+      <Text style= {styles.touchtext}>  MANAGE EMPLOYEES </Text>
+      </TouchableOpacity>
+    
+<TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("Order")}>
+      <Text style= {styles.touchtext}>  MANAGE ORDERS </Text>
+      </TouchableOpacity>
+    
     </View>
-    </View>
+  );
+};
+
+
+var productitems = [
+
+  {
+   
+    itemname: 'SWORD',
+    
+    cost: '1300',
+    image: 'https://i.pinimg.com/originals/f3/18/5f/f3185f78fcb64b2319aeb48ecd5a8bc1.jpg'
+      ,
+  },
+  {
+   
+    itemname: 'BLADE',
+    cost: '3500',
+    
+    image:
+      'https://i.pinimg.com/originals/2a/21/c9/2a21c96c51c562837e212ee109007f58.jpg'
+  },
+  {
+   
+    itemname: 'ODM GEAR',
+    cost: '3750',
+    image:
+    'https://cdna.artstation.com/p/assets/images/images/007/218/992/large/ryan-bradley-ryan-bradley-odm2.jpg?1504554199'
+  },
+
+];
+
+var members = [
+
+  {
+   
+    name: 'JEAN KIRSTEIN',
+    height: '175 cm',
+    image: 'https://i.pinimg.com/originals/c9/93/65/c99365f329b7c82cf52beeb837fc52a5.jpg'
+      ,
+  },
+  {
+   
+    name: 'MIKASA ACKERMAN',
+    height: '165cm',
+    image:
+      'https://media.comicbook.com/2020/07/attack-on-titan-mikasa-anime-1227615-1280x0.jpeg'
+  },
+  {
+   
+    name: 'LEVI ACKERMAN',
+    height: '160 cm',
+    image:
+'https://pbs.twimg.com/profile_images/1377892223564578816/U8jqzlcS.jpg'  },
+
+];
+
+
+var orders = [
+
+ {
+    orderID: 1,
+    productname : 'swords',
+    quantity: 3,
+    status: 'order placed',
+    date: 'January 1, 2005',
+  },
+  {
+    orderID: 2,
+    productname : 'blade',
+    quantity: 2
+    ,
+    status: 'order placed',
+    date: 'May 21, 2006',
+  },
+  {
+    orderID: 3,
+    productname : 'ODM Gear',
+    quantity: 1
+    ,
+    status: 'purchase successful',
+    date: 'June 11, 2016',
+  },
+ 
+];
+
+
+
+
+const ProdList = ({ navigation}) => {
+  return (
+     <ScrollView>
+        {productitems.map((item) => {
+          return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("Details", {item})}>
+      <Text style={styles.touchtxt}>{item.itemname}</Text>
+      </TouchableOpacity>
+
 
     
+      
+    </View>
+       );
+        })}
+      </ScrollView>
+  );
+};
+
+const ProdDetails = ({ route, navigation }) => {
+  const { item } = route.params;
+     
+       
+          return (
+    <View style={styles.container}>
+      
+      <Text style={{ fontWeight: 'bold' }}>{item.itemname}</Text>
+      <Text style={{ fontWeight: 'bold' }}>COST: {item.cost}</Text>
+       <Image style={styles.imagenew} source={{ uri: item.image }} />
+      
+
+
+    
+      
+    </View>
+       );
+      
+
+};
+const EmployeePage = ({ navigation}) => {
+return (
+     <ScrollView>
+        {members.map((item) => {
+          return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("Employee Details ", {item})}>
+      <Text style={styles.touchtxt}>{item.name}</Text>
+      </TouchableOpacity>
+
+
+    </View>
+       );
+        })}
+      </ScrollView>
+  );
+};
+
+const EmployeeDetailsPage = ({ route, navigation }) => {
+const { item } = route.params;
+     
+       
+          return (
+    <View style={styles.container}>
+      
+      <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
+      <Text >HEIGHT: {item.height}</Text>
+       <Image style={styles.imagenew} source={{ uri: item.image }} />
+      
+
+
+    
+      
+    </View>
+       );
+};
+
+
+const OrderPage = ({ navigation}) => {
+ return (
+     <ScrollView>
+        {orders.map((item) => {
+          return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.touchstyle} onPress={() => navigation.navigate("Order Details", {item})}>
+      <Text style={styles.touchtxt}>{item.productname}</Text>
+      </TouchableOpacity>
+
+
+    </View>
+       );
+        })}
+      </ScrollView>
+  );
+};
+const OrderInfo = ({ route, navigation }) => {
+  const { item } = route.params;
+  return (
+     <View style={styles.container}>
+      
+      <Text style={{ fontWeight: 'bold' }}>NAME: {item.productname}</Text>
+      <Text style={{ fontWeight: 'bold' }}>ORDER ID: {item.orderID}</Text>
+       <Text style={{ fontWeight: 'bold' }}>QUANTITY: {item.quantity}</Text> 
+       <Text style={{ fontWeight: 'bold' }}>DATE: {item.date}</Text>
+        <Text style={{ fontWeight: 'bold' }}>STATUS: {item.status}</Text>
+      
+
+
+    
+      
+    </View>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="List of Products" component={ProdList} />
+        <Stack.Screen name="Details" component={ProdDetails} />
+        <Stack.Screen name="Employee" component={EmployeePage} />
+        <Stack.Screen name="Employee Details " component={EmployeeDetailsPage} />      
+        <Stack.Screen name="Order" component={OrderPage} />
+        <Stack.Screen name="Order Details" component={OrderInfo} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+
+  touchstyle: {    
+    backgroundColor: 'crimson',
     padding: 10,
-    margin: 10,
-    color: 'brown',
-    borderWidth: '2px',
-    borderColor: 'purple',
+    borderRadius: 25,
+    marginTop: 20
   },
-  viewRowContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'yellow',
-    width: '100%',
-    borderColor: '#c2185b',
-    borderWidth: '1px',
-    cursor: 'pointer',
+  touchtext:{
+    marginTop: 10, 
+    marginBottom: 10
   },
-   centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+ 
+imagenew: {
+     width: '100px',
+    height: '100px',
   },
-  btnSize: { width: '100px', textAlign: 'center' },
-
-  buttonText: { fontSize: 30, color: '#c2185b' },
-  buttonContainer: {
-    width: '100%',
-    height: 100,
-    borderWidth: '1px',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 30,
-  }, modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-
-  
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  }
 });
+export default App;
